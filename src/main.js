19582f6ae7be65e57ea30d821923969725dee13e
birando <birger1@live.com>
2024-10-06 23:07:48 +0200

    feat: add declaration of interactive elements  Refs: #3

--- src/main.js
+++ src/main.js
@@ -1,1 +1,38 @@
 import "./style.css";
+
+const gameModeText = document.querySelector(".game-mode-text");
+const playerTurnMessage = document.querySelector(".player-turn-message");
+const topHeader = document.querySelector(".top-header");
+const bottomHeader = document.querySelector(".bottom-header");
+const pvpButton = document.querySelector(".pvp-button");
+const pvcButton = document.querySelector(".pvc-button");
+const replayButton = document.querySelector(".replay-button");
+const upperPlayerName = document.querySelector(".upper-player-name");
+const upperPlayerScore = document.querySelector(".upper-player-score");
+const lowerPlayerName = document.querySelector(".lower-player-name");
+const lowerPlayerScore = document.querySelector(".lower-player-score");
+const upperPlayerBoard = document.querySelector(".upper-player-board");
+const upperBoardNumberColumn = document.querySelector(
+  ".upper-board-number-column"
+);
+const lowerBoardLetterRow = document.querySelector(".upper-board-letter-row");
+const lowerPlayerBoard = document.querySelector(".upper-player-board");
+const lowerBoardNumberColumn = document.querySelector(
+  ".lower-board-number-column"
+);
+const upperBoardLetterRow = document.querySelector(".upper-board-letter-row");
+const upperPlayerShips = document.querySelector(".upper-player-ships");
+const upperPlayerShip1 = document.querySelector(".upper-player-ship1");
+const upperPlayerShip2 = document.querySelector(".upper-player-ship2");
+const upperPlayerShip3 = document.querySelector(".upper-player-ship3");
+const upperPlayerShip4 = document.querySelector(".upper-player-ship4");
+const upperPlayerShip5 = document.querySelector(".upper-player-ship5");
+const lowerPlayerShips = document.querySelector(".lower-player-ships");
+const lowerPlayerShip1 = document.querySelector(".lower-player-ship1");
+const lowerPlayerShip2 = document.querySelector(".lower-player-ship2");
+const lowerPlayerShip3 = document.querySelector(".lower-player-ship3");
+const lowerPlayerShip4 = document.querySelector(".lower-player-ship4");
+const lowerPlayerShip5 = document.querySelector(".lower-player-ship5");
+// board squares (after creating the board in JS)
+
+
