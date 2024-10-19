e7ce9ab83a6e858864f1f066e58daa4f4e21efe0
birando <birger1@live.com>
2024-10-19 12:24:27 +0200

    add first shipPlacement

--- src/model.js
+++ src/model.js
@@ -2,10 +2,10 @@ import { ViewModel } from "./viewModel";
 
 export function Model() {
   class Ship {
-    constructor(name, life, size) {
-      this.name = name;
+    constructor(shipType, life, color) {
+      this.shipType = shipType;
       this.life = life;
-      this.size = size;
+      this.color = color;
       this.destroyed = false;
       this.boardPlacement = [];
     }
@@ -20,9 +20,6 @@ export function Model() {
         this.destroyed = true;
       }
     }
-    setBoardPlacement(position) {
-      this.boardPlacement = position;
-    }
     isHitAtPosition(x, y) {
       return this.boardPlacement.some(
         (position) => position.x === x && position.y === y
@@ -30,21 +27,11 @@ export function Model() {
     }
   }
 
-  function createShips() {
-    const player1Ship1 = new Ship("carrier", 5, 5);
-    const player1Ship2 = new Ship("battleship", 4, 4);
-    const player1Ship3 = new Ship("cruiser", 3, 3);
-    const player1Ship4 = new Ship("submarine", 3, 3);
-    const player1Ship5 = new Ship("destroyer", 2, 2);
-
-    const player2Ship1 = new Ship("carrier", 5, 5);
-    const player2Ship2 = new Ship("battleship", 4, 4);
-    const player2Ship3 = new Ship("cruiser", 3, 3);
-    const player2Ship4 = new Ship("submarine", 3, 3);
-    const player2Ship5 = new Ship("destroyer", 2, 2);
+  function createShip(shipType, life, color) {
+    return new Ship(shipType, life, color);
   }
 
   return {
-    createShips,
+    createShip,
   };
 }
