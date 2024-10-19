35a5287c245d0caf6a2486b767113e2be2ae8672
birando <birger1@live.com>
2024-10-19 12:24:27 +0200

    add first shipPlacement

--- src/viewModel.js
+++ src/viewModel.js
@@ -1,6 +1,14 @@
 //* VIEW MODEL - INTERFACE (UI)
+import { Model } from "./model";
+const CARRIER_SIZE = 5;
+const BATTLESHIP_SIZE = 4;
+const CRUISER_SIZE = 3;
+const SUBMARINE_SIZE = 3;
+const DESTROYER_SIZE = 2;
+let arrangeMode = false;
 
 export function ViewModel() {
+  //* API for elements
   const gameModeText = document.querySelector(".game-mode-text");
   const playerTurnMessage = document.querySelector(".player-turn-message");
   const topHeader = document.querySelector(".top-header");
@@ -36,7 +44,29 @@ export function ViewModel() {
   const lowerPlayerShip3 = document.querySelector(".lower-player-ship3");
   const lowerPlayerShip4 = document.querySelector(".lower-player-ship4");
   const lowerPlayerShip5 = document.querySelector(".lower-player-ship5");
+
+  const upperPlayerSquares = [];
+  const lowerPlayerSquares = [];
+
+  const shipInputBox = document.querySelector(".ship-input-box");
+  const lowerShipInputButton1 = document.querySelector(
+    ".lower-ship-input-button1"
+  );
   const contentContainer = document.querySelector(".content");
+
+  //* API for Ships
+  const player1Ship1 = Model().createShip("carrier", 5, "blue");
+  const player1Ship2 = Model().createShip("battleship", 4, "red");
+  const player1Ship3 = Model().createShip("cruiser", 3, "green");
+  const player1Ship4 = Model().createShip("submarine", 3, "gray");
+  const player1Ship5 = Model().createShip("destroyer", 2, "yellow");
+  const player2Ship1 = Model().createShip("carrier", 5, "blue");
+  const player2Ship2 = Model().createShip("battleship", 4, "red");
+  const player2Ship3 = Model().createShip("cruiser", 3, "green");
+  const player2Ship4 = Model().createShip("submarine", 3, "gray");
+  const player2Ship5 = Model().createShip("destroyer", 2, "yellow");
+  //* API END
+  //* -----------------------------------------------------------------------------
 
   function clearContent() {
     for (let i = 0; i < contentContainer.children.length; i++) {
@@ -68,12 +98,71 @@ export function ViewModel() {
     }
   }
 
+  function shipArrangement() {
+    playerTurnMessage.style.display = "flex";
+    shipArrangementPlayer1();
+  }
+
+  function shipArrangementPlayer1() {
+    playerTurnMessage.textContent = "PLAYER 1 - ARRANGE SHIPS";
+    upperBoardArea.style.opacity = "50%";
+    shipInputBox.style.display = "flex";
+  }
+
+  function placeShip(ship) {
+    ship.boardPlacement = calculatePlacement(ship);
+    // checkIfPositionIsTaken(ship);
+    setShipPlacementColors(ship.boardPlacement, ship.color);
+    console.log(ship.color);
+  }
+
+  function setShipPlacementColors(coordinates, color) {
+    coordinates.forEach((coordinate) => {
+      for (let i = 0; i < lowerPlayerSquares.length; i++) {
+        if (lowerPlayerSquares[i].getAttribute("id") == coordinate.toString()) {
+          lowerPlayerSquares[i].style.background = color; // Color the square
+        }
+      }
+    });
+  }
+
+  function calculatePlacement(ship) {
+    //Choosing a random coordinate to begin the placement from
+    const coordinate = Math.floor(Math.random() * 100);
+    //Choosing vertical or horizontal placement randomly
+    const directionOfPlacement = Math.floor(Math.random() * 2);
+    const placement = [];
+
+    for (let i = 0; i < ship.life; i++) {
+      //Vertical placement
+      if (directionOfPlacement == 0) {
+        if (coordinate <= 59) placement.push(coordinate + i * 10);
+        else placement.push(coordinate - i * 10);
+        //Horizontal placement.
+      } else {
+        // If coordinate ends with 5 or less:
+        if (coordinate % 10 <= 5) placement.push(coordinate + i);
+        else placement.push(coordinate - i);
+      }
+    }
+    console.log(placement);
+    return placement;
+  }
+
+  // function checkIfPositionIsTaken(ship){
+  //   const allPlacements = []
+  // }
+
   function bindEvents() {
     pvpButton.addEventListener("click", () => {
       gameModeText.textContent = "Mode: PvP";
       upperPlayerName.textContent = "Player 2";
       clearContent();
       displayGameScreen();
+      shipArrangement();
+    });
+    lowerShipInputButton1.addEventListener("click", () => {
+      placeShip(player1Ship1);
     });
   }
 
@@ -88,6 +177,11 @@ export function ViewModel() {
         // Create a matrix item (div)
         const matrixItem = document.createElement("div");
         matrixItem.classList.add("matrix-item");
+
+        //Set up ID
+        matrixItem.setAttribute("id", row.toString() + col.toString());
+
+        upperPlayerSquares.push(matrixItem);
 
         // Create a dot (div) for the center
         const dot = document.createElement("div");
@@ -104,6 +198,11 @@ export function ViewModel() {
         const matrixItem2 = document.createElement("div");
         matrixItem2.classList.add("matrix-item");
 
+        //Set up ID
+        matrixItem2.setAttribute("id", row.toString() + col.toString());
+
+        lowerPlayerSquares.push(matrixItem2);
+
         // Create a dot (div) for the center
         const dot2 = document.createElement("div");
         dot2.classList.add("dot");
@@ -116,6 +215,7 @@ export function ViewModel() {
       }
     }
   }
+
   return {
     displayHomeScreen,
     bindEvents,
